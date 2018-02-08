69074f04b716e2e044d3d39f95b4895276a00d66
chenjunjialinss <343144557@qq.com>
2018-02-08 16:19:36 +0800

    百度地图和查询ip

---
+++ market/src/com/bdqn/utils/IpSearch.java
@@ -0,0 +1,36 @@
+package com.bdqn.utils;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author chenjunjia
+ * @version V1.0
+ * @Title: ${file_name}
+ * @Package ${package_name}
+ * @Description: ${todo}(用一句话描述该文件做什么)
+ * @date ${date} ${time}
+ */
+public class IpSearch {
+    /**
+     * 获取登录用户IP地址
+     *
+     * @param request
+     * @return
+     */
+    public static String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        if (ip.equals("0:0:0:0:0:0:0:1")) {
+            ip = "本地";
+        }
+        return ip;
+    }
+}
